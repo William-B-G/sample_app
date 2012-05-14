2e18697c252fc716b7ef5c8968c9ace04e5fafcd
William Grauer <William.Grauer@gmail.com>
2012-05-14 14:16:58 -0400

    Remove '-xi86' - Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '956bf048195f48ad6580d1f4721412e6112b43f0d68a1e6c429446ebdc9408cacee446f6e9321c60ccc9263cccc6c03911fe1dab126e87737daeb2d92cbd3345'
