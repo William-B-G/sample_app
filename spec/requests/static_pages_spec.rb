b19df33f72a258218d72959825e3bdcafaba0dfa
William Grauer <William.Grauer@gmail.com>
2012-05-25 13:29:49 -0400

    OK up to page 249 - Rails routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,71 +1,73 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
   describe "Home page" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get '/static_pages/home'
-      response.status.should be(200)
+
+    it "should have the h1 'Sample App'" do
+      # visit root_path
       visit '/static_pages/home'
-      page.should have_content("Sample App")
+      page.should have_selector('h1', text: 'Sample App')
     end
-    
+
     it "should have the base title" do
-      visit '/static_pages/home'    	
-      page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App")     	
-	end    
-	
-    it "should not have a custom page title " do
-      visit '/static_pages/home'    	
-      page.should_not have_selector('title', 
-      	:text => "| Home")     	
-	end  	
-    
+      # visit root_path
+      visit '/static_pages/home'
+      page.should have_selector('title',
+      text: "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      # visit root_path
+      visit '/static_pages/home'
+      page.should_not have_selector('title', text: '| Home')
+    end
   end
-  
-  describe "Contact page" do
-    it "should have h1 'Contact Me'" do
-      get '/static_pages/contact'
-      response.status.should be(200)
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact Me')
-    end
-    
-    it "should have the right title" do
-      visit '/static_pages/contact'    	
-      page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App | Contact")     	
-	end    
-    
-  end  
-  
+
   describe "Help page" do
-    it "should have the content 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get '/static_pages/help'
-      response.status.should be(200)
+
+    it "should have the h1 'Help'" do
+      # visit help_path
       visit '/static_pages/help'
-      page.should have_content("Help")
-      page.should have_selector('h1', :text => 'Help')           
+      page.should have_selector('h1', text: 'Help')
     end
-    
-    it "should have the right title" do
-      visit '/static_pages/help'    	
-      page.should have_selector('title', 
-      	:text => "Ruby on Rails Tutorial Sample App | Help")     	
-	end
-	
-  end  
-  
+
+    it "should have the title 'Help'" do
+      # visit help_path
+      visit '/static_pages/help'
+      page.should have_selector('title',
+      text: "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
   describe "About page" do
-    it "should have the content 'About Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get '/static_pages/about'
-      response.status.should be(200)
-      visit '/static_pages/about'
-      page.should have_content("About Us")
+
+    it "should have the h1 'About'" do
+      # visit about_path
+      #page.should have_selector('h1', text: 'About Us')
     end
-  end  
-    
-end
+
+    it "should have the title 'About Us'" do
+      # visit about_path
+      visit '/static_pages/about'
+      page.should have_selector('title',
+      text: "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      # visit contact_path
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      # visit contact_path
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+      text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+end  
